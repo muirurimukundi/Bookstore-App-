0f69237aaa2b23cf1ad54e6ad2eac4de0438d209
muirurimukundi <themasalacollective@gmail.com>
2024-09-03 15:15:22 -0400

    Add: include the database mongodb created on the cloud.

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+mongodb+srv://root:l4vwvppEnIxMK0AO@bookstoreapp.kvkpu.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookStoreApp
+
+// Please create a free database for yourself
+// Database will be deleted after tutorial
